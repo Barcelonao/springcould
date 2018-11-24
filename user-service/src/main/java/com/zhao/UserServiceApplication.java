b43e7d2e2796ae8122fbd636e622fe8d18df4da7
zhaoxy <meetzhao@outlook.com>
2018-11-24 16:10:57 +0800

    user-module ----> user-service

---
+++ user-service/src/main/java/com/zhao/UserServiceApplication.java
@@ -0,0 +1,21 @@
+package com.zhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/*
+ * @description 模拟登录服务提供者 3
+ *
+ * @author MR.Zhao
+ * @date 2018-11-04 下午5:23
+ * @version 1.0.0
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
